c2f03d2339fe90ded7bc06f7d3ddf15703320953
Salih <salihugur46@gmail.com>
2023-12-25 17:14:45 +0300

    fontfamily changed

--- components/MenuItem.jsx
+++ components/MenuItem.jsx
@@ -5,7 +5,7 @@ const MenuItem = ({ title, price, tags, img,extras,onClick }) => {
     <div className="relative w-full mt-4 justify-between">
       <div className="flex justify-between items-center">
         <div className="w-[90%]">
-          <p className="text-left font-alt text-[18px] font-black md:text-lg ">
+          <p className="text-left font-alt text-[16px] font-black md:text-lg ">
             {title}
           </p>
 
@@ -18,7 +18,7 @@ const MenuItem = ({ title, price, tags, img,extras,onClick }) => {
         <div className="flex justify-center items-center">
           <div className="mr-8 w-8 h-[1px] bg-secondary m-0 1rem" />
           <div className="flex justify-end items-end">
-            <p className="w-12 text-sm md:text-lg lg:text-lg font-base font-black text-[18px] text-secondary">
+            <p className="w-12 md:w-12 text-sm md:text-[16px] font-base font-black text-[18px] text-secondary">
               {price} ₺
             </p>
           </div>
@@ -26,14 +26,16 @@ const MenuItem = ({ title, price, tags, img,extras,onClick }) => {
       </div>
       {extras?.map((item, index) => (
         <div key={index} className="flex justify-between text-left text-sm font-alt font-black gap-2 mt-2">
-          <h4 className="flex gap-4">
+          <h4 className="flex gap-4 text-[12px] md:text-[16px]">
           {item.title}
-          <button onClick={()=> onClick(item)} className="text-white text-[12px] font-alt px-2 bg-secondary">Hikayesi</button>
+          <button onClick={()=> onClick(item)} className="text-white text-[12px] md:text-[14px] font-alt px-2 bg-secondary">
+            Hikayesi
+            </button>
           </h4>
           <div className="flex justify-center items-center">
             <div className="mr-8 w-4 h-[.5px] bg-secondary m-0 1rem" />
             <div className="flex justify-end items-end">
-              <p className="w-12 text-xs md:text-lg lg:text-lg font-base font-black text-[18px] text-secondary">
+              <p className="w-12 md:w-14 text-xs md:text-base font-base font-regular text-secondary">
                 + {item.price} ₺
               </p>
             </div>
